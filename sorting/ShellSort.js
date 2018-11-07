b9967b7e947a3812599098b60ef2b42170c78dfc
LuizGuerra <luizpedrofg@gmail.com>
2018-11-07 09:10:16 -0200

    Update sorting/ShellSort.js

--- sorting/ShellSort.js
+++ sorting/ShellSort.js
@@ -1,29 +1,13 @@
-/* "Shell sort or Shell's method, is an in-place comparison sort. 
-It can be seen as either a generalization of sorting by exchange (bubble sort) or sorting by insertion (insertion sort). 
-The method starts by sorting pairs of elements far apart from each other, then progressively reducing the gap between elements to be compared. 
-Starting with far apart elements can move some out-of-place elements into position faster than a simple nearest neighbor exchange." */
-
-function shellSort(arr) {
-    var increment = arr.length / 2;
-    while (increment > 0) {
-        for (i = increment; i < arr.length; i++) {
-            var j = i;
-            var temp = arr[i];
-    
-            while (j >= increment && arr[j-increment] > temp) {
-                arr[j] = arr[j-increment];
-                j = j - increment;
-            }
-    
-            arr[j] = temp;
-        }
-    
-        if (increment == 2) {
-            increment = 1;
-        } else {
-            increment = parseInt(increment*5 / 11);
-        }
+function shellsort(array) {
+  for(var g = 0; g < gaps.length; g++) {
+    var gap = gaps[g];
+    for(var i = gap; i < array.length; i++) {
+      var temp = array[i];
+      for(var j = i; j >= gap && array[j - gap] > temp; j -= gap) {
+        array[j] = array[j - gap];
+      }
+      array[j] = temp;
     }
-  return arr;
+  }
+  return array;
 }
-
